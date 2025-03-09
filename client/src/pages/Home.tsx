48cafa8e962871ea2835cd4b7c41d79df302fb70
Shuklashashi0706 <shuklashashi169@gmail.com>
2025-03-09 15:02:27 +0530

    asd

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -15,6 +15,7 @@ interface HomeProps {
   onPageChange: (page: "home" | "profile") => void;
 }
 
+
 const API_URL = import.meta.env.VITE_BACKEND_URI;
 
 export function Home({ onPageChange }: HomeProps) {
