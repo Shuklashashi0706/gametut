477391d3887e742b3737e447170cd55c045a1043
Shuklashashi0706 <shuklashashi169@gmail.com>
2025-03-13 21:17:21 +0530

    efdfdf

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -152,8 +152,8 @@ export function Home({ onPageChange }: HomeProps) {
 
         setSeedPairId(response.data.seedPairId);
         setServerSeedHash(response.data.serverSeedHash);
-        // setEncryptedCrashLane(response.data.encryptedCrashLane);
-        setEncryptedCrashLane(5);
+        setEncryptedCrashLane(response.data.encryptedCrashLane);
+        // setEncryptedCrashLane(5);
         setNonce(response.data.nonce);
         setError("");
       } catch (err: any) {
